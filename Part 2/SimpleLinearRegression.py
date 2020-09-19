cc5fa527a5a07760710bff93a3acf35cf0bf5894
ignacio Solaberrieta <igna.arg@hotmail.com>
2020-09-19 13:45:45 -0400

    adding the data file inside part 2

---
+++ Part 2/SimpleLinearRegression.py
@@ -0,0 +1,9 @@
+#importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
